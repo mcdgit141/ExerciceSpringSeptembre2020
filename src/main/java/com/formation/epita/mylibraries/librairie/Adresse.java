39a209559d46eb74176fb6dd0570debbd6e41909
mcdgit141 <mcdmail77@gmail.com>
2020-09-03 10:42:15 +0200

    premier commit

---
+++ src/main/java/com/formation/epita/mylibraries/librairie/Adresse.java
@@ -0,0 +1,56 @@
+package com.formation.epita.mylibraries.librairie;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
+@Embeddable
+public class Adresse {
+
+    private String numero;
+    private String rue;
+    private String ville;
+    private String codePostal;
+
+    public Adresse() {
+
+    }
+
+    public Adresse(String numero, String rue, String ville, String codePostal) {
+        this.numero = numero;
+        this.rue = rue;
+        this.ville = ville;
+        this.codePostal = codePostal;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getRue() {
+        return rue;
+    }
+
+    public void setRue(String rue) {
+        this.rue = rue;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getCodePostal() {
+        return codePostal;
+    }
+
+    public void setCodePostal(String codePostal) {
+        this.codePostal = codePostal;
+    }
+}
